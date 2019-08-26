61bdcc2053912021dbf4c759743c542b9b5308e1
Stavroula Guerrero <4stacyg@optonline.net>
2019-08-26 14:40:24 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,7 +34,7 @@ def select_series_title_with_most_human_characters
   "SELECT series.title
   FROM series
   JOIN books
-  ON books.series_id = series_id
+  ON books.series_id = series.id
   JOIN character_books
   ON character_books.book_id = books.id
   JOIN characters
